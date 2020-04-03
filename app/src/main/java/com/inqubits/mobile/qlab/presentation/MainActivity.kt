67cead01bc8e9de4f83975e64dd6addb77b3b29f
Hector Torres <torrhes@gmail.com>
2020-04-03 00:04:39 -0700

    Fixing flaw on adding a fragment

--- app/src/main/java/com/inqubits/mobile/qlab/presentation/MainActivity.kt
+++ app/src/main/java/com/inqubits/mobile/qlab/presentation/MainActivity.kt
@@ -7,14 +7,27 @@ import com.inqubits.mobile.qlab.R
 
 class MainActivity : AppCompatActivity() {
 
+    private val movieFragmentTag = MovieFragment.javaClass.canonicalName
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        navigateToFragment(MovieFragment.newInstance(Bundle()))
+        setContentView(R.layout.activity_main) // set the base view
+
+        // TODO: add this to a utility function
+        var fragment = supportFragmentManager.findFragmentByTag(movieFragmentTag)
+        fragment = (if (fragment == null) {
+            MovieFragment.newInstance(Bundle())
+        } else {
+            fragment as MovieFragment
+        })
+
+        if (!fragment.isAdded) {
+            navigateToFragment(fragment)
+        }
     }
 
     private fun navigateToFragment(fragment: Fragment) {
-        supportFragmentManager.beginTransaction().add(R.id.main_content, fragment).commit()
+        supportFragmentManager.beginTransaction().add(R.id.main_content, fragment, movieFragmentTag).commit()
     }
 }
